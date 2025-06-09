9af14fedf016203a5a1b2041dbac03e17c8005ec
FrankWkd <10802418+frankwkd@user.noreply.gitee.com>
2025-06-09 17:49:35 +0800

    update

---
+++ czos/Matches/202506/T1.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author    FrankWKD (wangkedi01)
+ * @Date      2025-06-09
+ * @Source
+ * @License   GNU General Public License 2.0
+ * @FileName  T1.cpp
+ * @FilePath  /media/frank/FrankW/_default/_Mine!/Working/code-spaces/czos/Matches/202506/T1.cpp
+ */
+
+// #pragma GCC optimize(3)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // ios::sync_with_stdio(false);
+    // cin.tie(0); cout.tie(0);
+
+    return 0;
+}
