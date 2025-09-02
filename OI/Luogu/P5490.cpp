d9cc9f0a7e1deb07dac49167119406259cacd3a6
FrankWkd-Pro <tongguo84@163.com>
2025-09-02 12:22:06 +0000

    add files update settings.json add cpp code snippets

---
+++ OI/Luogu/P5490.cpp
@@ -0,0 +1,34 @@
+/*
+ * > CPP Code Snippet <
+ * > Powered by Microsoft Visual Studio Code <
+ *
+ * @Author    FrankWKD (wangkedi01)
+ * @Date      2025-09-02
+ * @Network   "https://www.luogu.com.cn/problem/P5490"
+ * @License   GNU General Public License 2.0
+ * @Platform  [Frank]iMac Ubuntu Pro 24.04 LTS
+ * @FileName  P5490.cpp
+ * @FilePath  /workspaces/CodeSpaces/OI/Luogu/P5490.cpp
+ * @Solution  --
+ */
+
+// #pragma GCC optimize(3)
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 5;
+int n;
+struct line {
+    int s, t, tag, x;
+} line[N << 1];
+int a[N << 1];
+int main() {
+    // freopen("sample.in","r",stdin);
+    // freopen("sample.out","w",stdout);
+    // ios::sync_with_stdio(false);
+    // cin.tie(0); cout.tie(0);
+    // 咕咕咕
+
+    // fclose(stdin);
+    // fclose(stdout);
+    return 0;
+}
